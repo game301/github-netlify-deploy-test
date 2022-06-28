0878a49e8d3f1e74b9c9be667483098badcf80c6
game301 <hurkaparty@gmail.com>
2022-06-28 22:17:13 +0200

    js button console log

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 const funButton = document.querySelector("[data-fun]");
 
 funButton.addEventListener("click", () => {
+    console.log("Fun!");
     const testHeader = document.createElement("h2");
     testHeader.append();
 });
