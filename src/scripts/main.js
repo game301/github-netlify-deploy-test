31e64d9f68aab2130a316f2ea00c294c1906dca7
game301 <hurkaparty@gmail.com>
2022-06-28 22:36:51 +0200

    js append child test1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,8 @@ const funButton = document.querySelector("[data-fun]");
 funButton.addEventListener("click", () => {
     console.log("Fun!");
     const testHeader = document.createElement("h2");
+    const newContent = document.createTextNode("Hi there and greetings!");
+    testHeader.appendChild(newContent);
+
     testHeader.append();
 });
