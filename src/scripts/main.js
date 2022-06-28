936eac84244fac8a7de0264bba0ba6e30e742411
game301 <hurkaparty@gmail.com>
2022-06-28 23:30:05 +0200

    test9

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,7 @@ const funButton = document.querySelector("[data-fun]");
 funButton.addEventListener("click", () => {
     console.log("Fun!");
 
-    // create a new div element
-    const newDiv = document.createElement("div");
-
-    // and give it some content
-    const newContent = document.createTextNode("Hi there and greetings!");
-
-    // add the text node to the newly created div
-    newDiv.appendChild(newContent);
-
-    // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
-    document.body.insertBefore(newDiv, currentDiv);
+    const para = document.createElement("p");
+    para.innerText = "This is a paragraph";
+    funButton.appendChild(para);
 });
