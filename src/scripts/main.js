5f681f25d3979c2578d0abf3c340b881e5eac9cf
game301 <hurkaparty@gmail.com>
2022-06-28 23:01:26 +0200

    js button div create

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,8 @@ const funButton = document.querySelector("[data-fun]");
 
 funButton.addEventListener("click", () => {
     console.log("Fun!");
+
+    // create a new div element
     const newDiv = document.createElement("div");
 
     // and give it some content
@@ -11,8 +13,6 @@ funButton.addEventListener("click", () => {
     newDiv.appendChild(newContent);
 
     // add the newly created element and its content into the DOM
-    const currentDiv = document.getElementById("div1");
+    const currentDiv = document.querySelector("#test-div");
     document.body.insertBefore(newDiv, currentDiv);
-
-    testHeader.append();
 });
