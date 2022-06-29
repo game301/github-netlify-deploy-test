8d714351eecd06b698e281e13b296aa6c8fc4502
game301 <hurkaparty@gmail.com>
2022-06-30 00:33:13 +0200

    fetch test preparations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,10 +3,12 @@ const funButton = document.querySelector("[data-fun]");
 funButton.addEventListener("click", () => {
     console.log("Fun!");
 
+    // create paragraph onclick
     const para = document.createElement("p");
     para.innerText = "This is a paragraph";
 
     const divTest = document.querySelector("#div-test");
-
     divTest.appendChild(para);
+
+    // fetch data and display with DOM
 });
