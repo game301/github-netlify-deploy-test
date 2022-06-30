2c40cda6d14e8b5d4c9ac32b03d2500ad34aa712
game301 <hurkaparty@gmail.com>
2022-06-30 16:35:38 +0200

    fetch test with promises and async await

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,3 +12,31 @@ funButton.addEventListener("click", () => {
 
     // fetch data and display with DOM
 });
+/*
+fetch("https://reqres.in/api/users")
+    .then((res) => {
+        if (res.ok) {
+            console.log("SUCCESS");
+            res.json();
+        } else {
+            console.log("Not Succesful");
+        }
+    })
+    .then((data) => console.log(data))
+    .catch((error) => console.log("ERROR", error));
+*/
+
+async function getData() {
+    try {
+        const response = await fetch("https://reqres.in/api/users/23");
+        console.log("SUCCESS, Response received", response);
+        if (response.ok) {
+            const resJson = await response.json();
+            console.log("Succesfuly converted into json", resJson);
+        }
+    } catch (err) {
+        console.log("Not Succesful", err);
+    }
+}
+
+getData();
